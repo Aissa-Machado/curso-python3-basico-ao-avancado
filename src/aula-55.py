28838a4f24d8b603bd996df147d7abbc679503f3
aissamachado3 <aissamachado3@gmail.com>
2025-05-22 16:35:51 -0300

    aulas

---
+++ src/aula-55.py
@@ -0,0 +1,4 @@
+import random
+
+cpf_aleatorio = random.randint(10000000000, 99999999999)
+print(cpf_aleatorio)
--- src/aula=52.py
+++ src/aula=52.py
@@ -1,5 +1,10 @@
-nome = 'Aissa','pedro','carlos'
+# nome = 'Aissa','pedro','carlos'
 # nomes[1] = 'outro'
-lista = list(nome)
-lista[1] = 'outro'
-print(lista)
+# lista = list(nome)
+# lista[1] = 'outro'
+# print(lista)
+
+import random
+numero = 123456789
+cpf_aleatorio = random.choice(numero)
+print(cpf_aleatorio)
